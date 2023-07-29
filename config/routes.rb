aac0fa6bc6256e51eb9ce1d5fb67fd42f58d0300
austinxduong <austinxduong@gmail.com>
2023-07-29 16:29:44 -0700

    create/configure route + controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #this maps the apps root route, to the index action method within the AccountsController (accounts_controller.rb)
+  root "accounts#index"
+
+  get "/accounts", to:"accounts#index"
 end
