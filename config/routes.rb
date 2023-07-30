ea84d64086eb7459fc1d3e7198566c97789db31f
austinxduong <austinxduong@gmail.com>
2023-07-29 18:35:00 -0700

    refactor routing with ruby route resources method

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   #this maps the apps root route, to the index action method within the AccountsController (accounts_controller.rb)
   root "accounts#index"
 
-  get "/accounts", to:"accounts#index"
-  get "/accounts/:id", to:"accounts#show"
+  #get "/accounts", to:"accounts#index"#
+  #get "/accounts/:id", to:"accounts#show"#
+
+  #resource method provides pre-made mapped "_url" and _"path" helpers" for conventional CRUD routes
+  resources :accounts
+
 end
