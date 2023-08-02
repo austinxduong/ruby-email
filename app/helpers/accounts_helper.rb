6ab7cacf7c42789e454b4ef17ef71f393a6d6409
austinxduong <austinxduong@gmail.com>
2023-08-01 19:57:53 -0700

    css validation

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,2 +1,12 @@
 module AccountsHelper
+    def alert_class(flash_type)
+        case flash_type.to.sym
+        when :notice
+            "alert-success"
+        when :alert
+            "alert-warning"
+        when :error
+            "alert-danger"
+        end
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    def alert_class(flash_type)
+        case flash_type.to.sym
+        when :notice
+            "alert-success"
+        when :alert
+            "alert-warning"
+        when :error
+            "alert-danger"
+        end
+    end
 end
+
